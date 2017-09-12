d40ae6ee68efdcad18319e30a3b5a0a3a93e349c
yang.liao <yang.liao@DESKTOP-2AUF4H3>
2017-09-12 23:04:14 +0800

    添加Bundle引用

--- Site.WeiXin.Manager/Global.asax.cs
+++ Site.WeiXin.Manager/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Site.WeiXin.Manager
@@ -13,6 +14,7 @@ namespace Site.WeiXin.Manager
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
